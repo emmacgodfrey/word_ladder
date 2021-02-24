5af5c3a6a71eb339c1592e62696abd0d7339116c
emmacgodfrey <emma.godfrey@pomona.edu>
2021-02-23 18:08:28 -0800

    first commit

--- word_ladder.py
+++ word_ladder.py
@@ -1,5 +1,8 @@
 #!/bin/python3
 
+from copy import copy, deepcopy
+from collections import deque
+
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     '''
@@ -16,18 +19,45 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     ```
     may give the output
     ```
-    ['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money']
+    ['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny',
+    'boney', 'money']
     ```
     but the possible outputs are not unique,
     so you may also get the output
     ```
-    ['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey', 'honey', 'money']
+    ['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey',
+    'honey', 'money']
     ```
     (We cannot use doctests here because the outputs are not unique.)
 
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
+    if start_word == end_word:
+        return [start_word]
+
+    with open('words5.dict') as f:
+        lines = [line.strip() for line in f]
+
+    stack = []
+    stack.append(start_word)
+    q = deque()
+    q.append(stack)
+
+    while not len(q) == 0:
+        current_stack = q.popleft()
+        copylines = copy(lines)
+        for word in copylines:
+            if _adjacent(word, current_stack[-1]):
+                if word == end_word:
+                    current_stack.append(word)
+                    return current_stack
+                copystack = deepcopy(current_stack)
+                copystack.append(word)
+                q.append(copystack)
+                lines.remove(word)
+    return None
+    f.close()
 
 
 def verify_word_ladder(ladder):
@@ -40,6 +70,17 @@ def verify_word_ladder(ladder):
     >>> verify_word_ladder(['stone', 'shone', 'phony'])
     False
     '''
+    if ladder is None or len(ladder) == 0:
+        return False
+
+    if len(ladder) == 1:
+        return True
+
+    for i in range(len(ladder) - 1):
+        if not _adjacent(ladder[i], ladder[i + 1]):
+            return False
+
+    return True
 
 
 def _adjacent(word1, word2):
@@ -52,3 +93,12 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+    if not len(word1) == len(word2):
+        return False
+
+    count = 0
+    for i in range(len(word1)):
+        if word1[i] != word2[i]:
+            count += 1
+
+    return count == 1
