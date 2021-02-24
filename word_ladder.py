b9584a05db60da91e495c7e81110cf7b5cf4514a
emmacgodfrey <emma.godfrey@pomona.edu>
2021-02-23 18:10:15 -0800

    second try

--- word_ladder.py
+++ word_ladder.py
@@ -6,7 +6,7 @@ from collections import deque
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     '''
-    Returns a list satisfying the following properties:
+    returns a list satisfying the following properties:
 
     1. the first element is `start_word`
     2. the last element is `end_word`
